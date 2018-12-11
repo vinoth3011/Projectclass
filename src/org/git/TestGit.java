b5c6926c7602aa3d341f5bd5b9dcf691de2ee8ef
vinoth3011 <nov.vino@gmail.com>
2018-12-11 10:29:20 +0530

    Added Code edited

--- src/org/git/TestGit.java
+++ src/org/git/TestGit.java
@@ -3,7 +3,10 @@ package org.git;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 30;
+		
+		System.out.println("a + b");
 
 	}
 
